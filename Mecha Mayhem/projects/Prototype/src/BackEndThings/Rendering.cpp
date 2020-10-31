4ddc62bf1d61302980e7e83ffe1e123c828ac58b
Jonathan Jay <jonathan.jay@ontariotech.net>
2020-10-30 21:32:58 -0400

    made rendering have more variables to edit
    lightcolour, default colour and light pos, check rendering for includes

--- Mecha Mayhem/projects/Prototype/src/BackEndThings/Rendering.cpp
+++ Mecha Mayhem/projects/Prototype/src/BackEndThings/Rendering.cpp
@@ -1,26 +1,38 @@
 #include "Rendering.h"
 
-void Rendering::Update(entt::registry* reg)
-{
-    glClearColor(BackColour.x, BackColour.y, BackColour.z, BackColour.w);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+namespace Rendering {
 
-    auto objView = reg->view<ObjLoader, Transform>();
-    auto cameraView = reg->view<Camera, Transform>();
-
-    for (auto cam : cameraView)
+    void Update(entt::registry* reg)
     {
-        cameraView.get<Camera>(cam).SetPosition(cameraView.get<Transform>(cam).GetPosition());
-        cameraView.get<Camera>(cam).SetRotation(cameraView.get<Transform>(cam).GetRotation());
+        glClearColor(BackColour.x, BackColour.y, BackColour.z, BackColour.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //draw all the objs
-        for (auto entity : objView)
+        auto objView = reg->view<ObjLoader, Transform>();
+        auto cameraView = reg->view<Camera, Transform>();
+
+        for (auto cam : cameraView)
         {
-            Transform &trans = objView.get<Transform>(entity);
+            cameraView.get<Camera>(cam).SetPosition(cameraView.get<Transform>(cam).GetPosition());
+            cameraView.get<Camera>(cam).SetRotation(cameraView.get<Transform>(cam).GetRotation());
 
-            objView.get<ObjLoader>(entity).Draw(
-                cameraView.get<Camera>(cam), trans.GetModel(), trans.GetRotationM3(),
-                glm::vec3(1.f), cameraView.get<Transform>(cam).GetPosition(), glm::vec3(3.f));
+            Camera& camCam = cameraView.get<Camera>(cam);
+
+            LightPos = cameraView.get<Transform>(cam).GetPosition();
+
+            //draw all the objs
+            for (auto entity : objView)
+            {
+                Transform& trans = objView.get<Transform>(entity);
+
+                objView.get<ObjLoader>(entity).Draw(camCam, trans.GetModel(), trans.GetRotationM3(),
+                    DefaultColour, LightPos, LightColour);
+            }
         }
     }
+
+    glm::vec4 BackColour = { 0.2f, 0.2f, 0.2f, 1.f };
+    glm::vec3 LightColour = glm::vec3(3.f);
+    glm::vec3 LightPos = glm::vec3(0.f);
+    glm::vec3 DefaultColour = glm::vec3(1.f);
+
 }
