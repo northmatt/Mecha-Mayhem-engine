ca1bc8c82a7b21a918fe691c56c4282584fee950
Jonathan (Log-Hei) Leung <loghei.leung@ontariotechu.net>
2021-03-14 15:43:30 -0400

    Simple code in DemoScene.cpp

--- Mecha Mayhem/projects/Prototype/src/DemoScene.cpp
+++ Mecha Mayhem/projects/Prototype/src/DemoScene.cpp
@@ -188,6 +188,26 @@ void DemoScene::Update()
 		}
 		m_timer = 5.f;
 	}
+	if (Input::GetKeyDown(KEY::ONE))
+	{
+		std::cout << "Test one" << std::endl;
+	}
+	if (Input::GetKeyDown(KEY::TWO))
+	{
+		std::cout << "Test two" << std::endl;
+	}
+	if (Input::GetKeyDown(KEY::THREE))
+	{
+		std::cout << "Test three" << std::endl;
+	}
+	if (Input::GetKeyDown(KEY::FOUR))
+	{
+		std::cout << "Test four" << std::endl;
+	}
+	if (Input::GetKeyDown(KEY::FIVE))
+	{
+		std::cout << "Test five" << std::endl;
+	}
 }
 
 void DemoScene::LateUpdate()
